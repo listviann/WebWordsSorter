74f4acc7a2a139974234759de9444753d73784f0
listviann <mixail.larionov.2002@mail.ru>
2024-04-11 14:10:37 +0500

    first commit

---
+++ Client/Program.cs
@@ -0,0 +1,47 @@
+﻿using System.Net.Sockets;
+using System.Text.RegularExpressions;
+
+int port = 13000;
+string serverAddress = "127.0.0.1";
+
+if (args.Length > 0)
+    serverAddress = args[0];
+
+var tcpClient = new TcpClient(serverAddress, port);
+
+try
+{
+    var stream = tcpClient.GetStream();
+    var streamReader = new StreamReader(stream);
+    var streamWriter = new StreamWriter(stream)
+    {
+        AutoFlush = true
+    };
+
+    // Console.WriteLine(streamReader.ReadLine());
+
+    while (true)
+    {
+        Console.WriteLine("Words: ");
+        string? words = Console.ReadLine();
+        Console.WriteLine(words);
+        if (words is null || words == "") break;
+        streamWriter.WriteLine(words);
+        if (words == "") break;
+        Console.WriteLine("Sorted words: ");
+
+        var response = streamReader.ReadLine();
+        var formatResponse = response!.Replace("\t", Environment.NewLine);
+        Console.WriteLine(formatResponse);
+    }
+
+    stream.Close();
+}
+catch (Exception ex)
+{
+    Console.WriteLine($"ERROR: {ex.Message}");
+}
+finally
+{
+    tcpClient.Close();
+}
---
+++ Server/Program.cs
@@ -0,0 +1,64 @@
+﻿using System.Net;
+using System.Net.Sockets;
+using System.Text;
+
+const int LIMIT = 5;
+int port = 13000;
+var localAddress = IPAddress.Parse("127.0.0.1");
+var tcpListener = new TcpListener(localAddress, port);
+tcpListener.Start();
+Console.WriteLine($"Server mounted, listening to port {port}");
+
+// accept and serve more than one client
+for (int i = 0; i < LIMIT; i++)
+{
+    Thread t = new(new ThreadStart(Service));
+    t.Start();
+}
+
+Service();
+
+void Service()
+{
+    while (true)
+    {
+        Socket sock = tcpListener.AcceptSocket();
+        Console.WriteLine($"Connected {sock.RemoteEndPoint}");
+
+        try
+        {
+            var networkStream = new NetworkStream(sock);
+            var streamReader = new StreamReader(networkStream);
+            var streamWriter = new StreamWriter(networkStream)
+            {
+                AutoFlush = true
+            };
+
+            while (true)
+            {
+                string? message = streamReader.ReadLine();
+                Console.WriteLine(message);
+                if (message is null || string.IsNullOrWhiteSpace(message)) break;
+                string[] words = message.Split(' ');
+                Array.Sort(words);
+                var stringBuilder = new StringBuilder();
+
+                for (int i = 0; i < words.Length; i++)
+                    stringBuilder.Append(words[i] + "\t");
+
+                string responseMessage = stringBuilder.ToString();
+                Console.WriteLine(responseMessage);
+                streamWriter.WriteLine(responseMessage);
+            }
+
+            networkStream.Close();
+        }
+        catch (Exception ex)
+        {
+            Console.WriteLine($"ERROR: {ex.Message}");
+        }
+
+        Console.WriteLine($"Disconnected:: {sock.RemoteEndPoint}");
+        sock.Close();
+    }
+}
